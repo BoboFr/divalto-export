0293335679b4464a69b8202ddba52fd2c0a2b526
BoboFr <gaetan.biord@gmail.com>
2025-11-26 13:17:44 +0100

    Test: update

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -7,9 +7,9 @@ using System.Windows;
 [assembly: AssemblyProduct("Divalto")]
 [assembly: AssemblyCopyright("Copyright © Gaëtan BIORD 2025")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.1")] // Increment this version for each release build
 
-[assembly:ThemeInfo(
+[assembly: ThemeInfo(
     ResourceDictionaryLocation.None, //where theme specific resource dictionaries are located
                                      //(used if a resource is not found in the page,
                                      // or application resource dictionaries)
